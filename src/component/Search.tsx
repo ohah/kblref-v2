3c20698c9f38c015d72b7b27226c284d537e90a6
ohah <bookyoon173@gmail.com>
2021-04-30 18:31:53 +0900

    ㅕㅔㅇㅁㅅㄷ

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AutoComplete } from 'primereact/autocomplete';
 import { Router } from 'react-router';
 import { Link } from 'react-router-dom';
 
 export const Search = () => {
+  const [selectedCity, setSelectedCity] = useState<any>(null);
+  const [filteredCities, setFilteredCities] = useState<any>(null);
+
   const groupedCities = [
     {
       label: 'Germany', code: 'DE',
@@ -33,14 +36,40 @@ export const Search = () => {
       ]
     }
   ];
-  const data = [
-    { "name": "Afghanistan", "code": "AF" },
-  ]
+
+  useEffect(() => {
+    // setCountries(countryservice);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const searchCity = (event: { query: string }) => {
+    let query = event.query;
+    let _filteredCities = [];
+
+    for (let country of groupedCities) {
+      let filteredItems = country.items.filter((item) => item.label.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+      if (filteredItems && filteredItems.length) {
+        _filteredCities.push({ ...country, ...{ items: filteredItems } });
+      }
+    }
+
+    setFilteredCities(_filteredCities);
+  }
+
+  const groupedItemTemplate = (item: any) => {
+    return (
+      <div className="p-d-flex p-ai-center country-item">
+        <div>{item.label}</div>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <AutoComplete value={data} />
-      <Link to="/개인스탯" > 개인스탯 </Link>
+    <div className="p-my-2">
+      <span className="p-fluid p-my-2">
+      <AutoComplete value={selectedCity} placeholder="검색어를 입력하세요" suggestions={filteredCities} completeMethod={searchCity} field="label" optionGroupLabel="label" optionGroupChildren="items"optionGroupTemplate={groupedItemTemplate} onChange={(e) => setSelectedCity(e.value)} style={{width:'100%'}}/>
+      </span>
     </div>
   )
 }
+
 export default Search
